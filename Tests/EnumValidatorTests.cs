5a0610bb84ca30634d07e0f8ef0eb0df5a8c81c8
Jan Vícha <j.vicha@quadient.com>
2020-01-09 12:07:36 +0100

    Added unit tests

--- Tests/EnumValidatorTests.cs
+++ Tests/EnumValidatorTests.cs
@@ -25,5 +25,42 @@ namespace Tests
             var enumValue = (ClassicEnum) value;
             enumValue.IsValid().Should().BeFalse();
         }
+        
+        [TestCase(2)]
+        [TestCase(69)]
+        [TestCase(666)]
+        public void SpecificEnum_ValidValues(int value)
+        {
+            var enumValue = (SpecificEnum) value;
+            enumValue.IsValid().Should().BeTrue();
+        }
+        
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(7)]
+        public void SpecificEnum_InvalidValues(int value)
+        {
+            var enumValue = (SpecificEnum) value;
+            enumValue.IsValid().Should().BeFalse();
+        }
+        
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void FlagsEnum_ValidValues(int value)
+        {
+            var enumValue = (FlagsEnum) value;
+            enumValue.IsValid().Should().BeTrue();
+        }
+        
+        [TestCase(666)]
+        [TestCase(-1)]
+        [TestCase(8)]
+        public void FlagsEnum_InvalidValues(int value)
+        {
+            var enumValue = (FlagsEnum) value;
+            enumValue.IsValid().Should().BeFalse();
+        }
     }
 }
--- Tests/Enums/FlagsEnum.cs
+++ Tests/Enums/FlagsEnum.cs
@@ -5,8 +5,9 @@ namespace Tests.Enums
     [Flags]
     public enum FlagsEnum
     {
+        None = 0,
         Flag0 = 1 << 0,
         Flag1 = 1 << 1,
-        Flag2 = 1 << 2,
+        Flag2 = 1 << 2
     }
 }
